ab10fa07db08e9e761fdd8ce312713a389b36c0a
Fang Fang <fang_fang@outlook.com>
2021-04-16 00:36:34 +0200

    "second submission"

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,12 +4,15 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    // perform T independent experiments on an N-by-N grid
     private Percolation percolation;
     private double[] thresholds;
     private boolean ok;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
         thresholds = new double[T];
         for (int i = 0; i < T; i++) {
             this.percolation = pf.make(N);
@@ -24,30 +27,23 @@ public class PercolationStats {
         }
     }
 
-
-
-    // sample mean of percolation threshold
     public double mean() {
         return StdStats.mean(thresholds);
     }
+
     public double stddev() {
         return StdStats.stddev(thresholds);
     }
-    // sample standard deviation of percolation threshold
+
+
     public double confidenceLow() {
         return mean() - 1.96 * stddev() / Math.sqrt(thresholds.length);
     }
-    // low endpoint of 95% confidence interval
+
+
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(thresholds.length);
     }                                 // high endpoint of 95% confidence interval
 
-    public static void main(String[] args) {
-        PercolationStats ps = new PercolationStats(20, 2000, new PercolationFactory());
-        System.out.println(ps.mean());
-        System.out.println(ps.stddev());
-        System.out.println(ps.confidenceLow());
-        System.out.println(ps.confidenceHigh());
-    }
 
 }
