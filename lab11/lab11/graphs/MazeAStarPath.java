03454d5f737d7aa5b59b1480cdece4ac2552f1d9
Fang Fang <fang_fang@outlook.com>
2021-05-02 23:51:26 +0200

    "first submission"

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+import org.junit.Test;
+
+import java.util.Comparator;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +14,8 @@ public class MazeAStarPath extends MazeExplorer {
     private boolean targetFound = false;
     private Maze maze;
 
+    MinPQ<Integer> fringe;
+
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         maze = m;
@@ -16,11 +23,13 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        fringe = new MinPQ<>(new EdgeComparator());
+        fringe.insert(s);
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return manhattan(v);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -30,13 +39,50 @@ public class MazeAStarPath extends MazeExplorer {
     }
 
     /** Performs an A star search from vertex s. */
-    private void astar(int s) {
-        // TODO
+    private void astar() {
+
+        int current = fringe.delMin();
+        marked[current] = true;
+        while (current != t) {
+
+            for (int w : maze.adj(current)) {
+                if (distTo[w] > distTo[current] + 1) {
+                    distTo[w] = distTo[current] + 1;
+                    edgeTo[w] = current;
+                    fringe.insert(w);
+                }
+            }
+            current = fringe.delMin();
+            announce();
+            marked[current] = true;
+        }
+        marked[current] = true;
+        announce();
+    }
+
+    private int manhattan(int v) {
+        return Math.abs(maze.toX(t) - maze.toX(v)) + Math.abs(maze.toY(t) - maze.toY(v));
     }
 
     @Override
     public void solve() {
-        astar(s);
+        astar();
+    }
+
+    private class EdgeComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer v1, Integer v2) {
+            return (distTo[v1] + h(v1)) - (distTo[v2] + h(v2));
+        }
+    }
+
+    @Test
+    public void testComparator() {
+        edgeTo[10] = 1;
+        edgeTo[11] = 2;
+        edgeTo[12] = 3;
+
     }
 
 }
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
+
 /**
  *  @author Josh Hug
  */
@@ -10,15 +13,57 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private Maze maze;
+    private Stack<Integer> parents;
+    private boolean hasCycle;
+
     public MazeCycles(Maze m) {
         super(m);
+
+        maze = m;
+        s = maze.xyTo1D(1, 1);
+        parents = new Stack<>();
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s, 0);
     }
 
-    // Helper methods go here
+    private void dfs(int w, int parent) {
+        if (hasCycle) {
+            return;
+        }
+
+        announce();
+        marked[w] = true;
+        parents.push(w);
+        for (int v : maze.adj(w)) {
+            if (!marked[v] && !hasCycle) {
+                announce();
+                marked[v] = true;
+                dfs(v, w);
+            } else if (v != parent && !hasCycle) {
+                hasCycle = true;
+                drawCycle(v, parents);
+                return;
+            }
+        }
+
+    }
+
+    public void drawCycle(int w, Stack<Integer> stack) {
+        int last = stack.pop();
+        int current = w;
+        while (last != w) {
+            edgeTo[current] = last;
+            current = last;
+            last = stack.pop();
+        }
+        edgeTo[current] = last;
+        announce();
+    }
+
 }
 
